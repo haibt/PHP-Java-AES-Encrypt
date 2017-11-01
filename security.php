c2e1da344a9dfc5cf313c3779fb06afdab6cb121
Aqrun <aqruna@gmail.com>
2017-11-01 11:23:52 +0800

    upgrade to use openssl

--- security.php
+++ security.php
@@ -1,34 +1,23 @@
 <?php
+/**
+ * AES/CBC/PKCS5Padding   encrypt decrypt
+ * js: https://blog.zhengxianjun.com/online-tool/crypto/aes/
+ * 
+ */
 class Security {
-	public static function encrypt($input, $key) {
-		$size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB); 
-		$input = Security::pkcs5_pad($input, $size); 
-		$td = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_ECB, ''); 
-		$iv = mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND); 
-		mcrypt_generic_init($td, $key, $iv); 
-		$data = mcrypt_generic($td, $input); 
-		mcrypt_generic_deinit($td); 
-		mcrypt_module_close($td); 
-		$data = base64_encode($data); 
-		return $data; 
-	} 
 
-	private static function pkcs5_pad ($text, $blocksize) { 
-		$pad = $blocksize - (strlen($text) % $blocksize); 
-		return $text . str_repeat(chr($pad), $pad); 
-	} 
-
-	public static function decrypt($sStr, $sKey) {
-		$decrypted= mcrypt_decrypt(
-			MCRYPT_RIJNDAEL_128,
-			$sKey, 
-			base64_decode($sStr), 
-			MCRYPT_MODE_ECB
-		);
-		$dec_s = strlen($decrypted); 
-		$padding = ord($decrypted[$dec_s-1]); 
-		$decrypted = substr($decrypted, 0, -$padding);
-		return $decrypted;
-	}	
+	public static function encrypt($contents, $encryptKey){
+		$iv = $encryptKey;
+		$re = openssl_encrypt($contents, 'aes-128-cbc', $encryptKey, OPENSSL_RAW_DATA, $iv);
+		$re = base64_encode($re);
+		return $re;
+	}
+	
+	public static function decrypt($contents, $encryptKey){
+		$iv = $encryptKey;
+		$contents = base64_decode($contents);
+		$re = openssl_decrypt($contents, 'aes-128-cbc', $encryptKey, OPENSSL_RAW_DATA, $iv);
+		return $re;
+	}
 }
 ?>
